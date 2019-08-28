8fbf7ae26276795cadc885385ab56ddf53a54eaa
Marco Antonio <marcoantoniozxcv@gmail.com>
2019-08-27 22:33:38 -0300

    aula 8 final

---
+++ src/main/java/com/example/demo/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Order;
+import com.example.demo.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		 Optional<Order> obj = repository.findById(id);
+		 return obj.get();
+	}
+}
