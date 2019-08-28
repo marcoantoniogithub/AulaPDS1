26f92d5f3e96a918dfb71c2e59497164d260d4f7
Marco Antonio <marcoantoniozxcv@gmail.com>
2019-08-27 22:03:22 -0300

    teste config git

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -15,7 +15,7 @@ import javax.persistence.Table;
 @Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+	 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
