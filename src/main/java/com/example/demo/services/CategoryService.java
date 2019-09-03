4841641aa3018701b6e68fcb3dead449c0e5e60b
EsthelaHesseLisboa <esthelalisboa@gmail.com>
2019-09-03 20:25:11 -0300

    Category

---
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Category;
+import com.example.demo.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		 Optional<Category> obj = repository.findById(id);
+		 return obj.get();
+	}
+}
