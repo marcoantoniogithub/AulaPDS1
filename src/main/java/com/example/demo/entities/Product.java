fdd2c259099bf27051136cfc065d20fad808a7fe
EsthelaHesseLisboa <esthelalisboa@gmail.com>
2019-09-03 21:40:25 -0300

    criado Product

---
+++ src/main/java/com/example/demo/entities/Product.java
@@ -0,0 +1,12 @@
+package com.example.demo.entities;
+
+public class Product {
+
+	private long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	
+}
