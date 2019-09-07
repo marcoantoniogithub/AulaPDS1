56fb8d84dc579bf100f0fa82a4b7b702c1f81b94
Lferreira13 <larissalara1972@gmail.com>
2019-09-06 21:15:51 -0300

    Product

--- src/main/java/com/example/demo/entities/Category.java
+++ src/main/java/com/example/demo/entities/Category.java
@@ -1,12 +1,15 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -17,6 +20,9 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -44,6 +50,10 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -64,7 +74,5 @@ public class Category implements Serializable{
 		if (id != other.id)
 			return false;
 		return true;
-	}
-
-	
+	}	
 }
