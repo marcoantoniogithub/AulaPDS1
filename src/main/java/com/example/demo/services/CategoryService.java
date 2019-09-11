5aad5f103da9b6eef8425e077ad627c9c525eb63
kethelemiftm <kethelemiftm@gmail.com>
2019-09-10 21:52:33 -0300

    CRUD categories

--- src/main/java/com/example/demo/services/CategoryService.java
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -3,11 +3,17 @@ package com.example.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.Category;
 import com.example.demo.repositories.CategoryRepository;
+import com.example.demo.services.exceptions.DatabaseException;
+import com.example.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,5 +28,33 @@ public class CategoryService {
 	public Category findById(Long id) {
 		 Optional<Category> obj = repository.findById(id);
 		 return obj.get();
+	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
