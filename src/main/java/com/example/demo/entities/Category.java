d589276ce135130e489892cea7cc58a0f1e49a46
Felipe Braz <felipe.tbraz@gmail.com>
2019-10-08 21:49:39 -0300

    Auditing data: PreUpdate & PrePersist

--- src/main/java/com/example/demo/entities/Category.java
+++ src/main/java/com/example/demo/entities/Category.java
@@ -1,6 +1,7 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,23 +10,28 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
-public class Category implements Serializable{
+public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
-	
+
+	private Instant createdAt;
+	private Instant updatedAt;
+
 	public Category() {
-		
+
 	}
 
 	public Category(Long id, String name) {
@@ -53,7 +59,27 @@ public class Category implements Serializable{
 	public Set<Product> getProducts() {
 		return products;
 	}
-	
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdateAt() {
+		return updatedAt;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		Instant now = Instant.now();
+		updatedAt = now;
+		createdAt = now;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -74,5 +100,5 @@ public class Category implements Serializable{
 		if (id != other.id)
 			return false;
 		return true;
-	}	
+	}
 }
